0df396eb8db7c21620075fb740e75973f1600045
Sunny Deng <sunnydeng.work@gmail.com>
2023-02-08 03:00:12 +0000

    add some files to ignore

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -4,24 +4,24 @@ locals {
 
 variable "gcp-credentials_path" {
   description = "Path to GCP credentials file"
-  type = string
-  default = "/Users/sunnyd/Downloads/shaped-manifest-376405-a15fbf66ab97.json"
+  type        = string
+  default     = "/Users/sunnyd/Downloads/shaped-manifest-376405-a15fbf66ab97.json"
 }
 
 variable "gcp_project_id" {
   description = "GCP Project ID"
-  type = string
-  default = "shaped-manifest-376405"
+  type        = string
+  default     = "shaped-manifest-376405"
 }
 
 variable "gcp_region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "us-west1"
-  type = string
+  default     = "us-west1"
+  type        = string
 }
 
 variable "bigquery_dataset" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
-  type = string
-  default = "ny_taxi_trips"
+  type        = string
+  default     = "ny_taxi_trips"
 }
